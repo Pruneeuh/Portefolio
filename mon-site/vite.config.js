0a8e02564a4d42657ab8ba25ce7e190a94ba3315
Pruneeuh <prune.mamalet@etu.inp-n7.fr>
2025-07-21 19:39:50 +0000

    Add logo n7

--- mon-site/vite.config.js
+++ mon-site/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // REMPLACEZ 'mon-eportfolio' par le nom exact de votre dépôt GitHub !
-  base: '/Portefolio/',
 })
