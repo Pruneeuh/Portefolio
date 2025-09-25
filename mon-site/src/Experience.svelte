b9d330eca53f68b1d6edf1987f60e84e8ac8fb0b
Pruneeuh <prune.mamalet@etu.inp-n7.fr>
2025-09-25 11:58:45 +0000

    Started experience page

--- mon-site/src/Experience.svelte
+++ mon-site/src/Experience.svelte
@@ -1,1 +1,8 @@
 <h1>Experience </h1>
+<div style="display: flex; align-items: center; gap: 1em;">
+  <div>
+    <h2 style="margin: 0;">Intership at ANITI</h2>
+    <p style="margin: 0;">I had the chance to realize my first intership at ANITI </p>
+  </div>
+  <img src="https://aniti.univ-toulouse.fr/wp-content/uploads/2024/06/01-FULL-bleu.png" alt="Logo ANITI" style="height: 8em; width: auto;" />
+</div>
